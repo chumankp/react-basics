0ccfcd9f7a1210eab2cd8d314de78684b53086e7
chuman <chuman.panda@gmail.com>
2023-07-26 19:01:58 +0530

    add

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@
 import './App.css';
 import Greeting from './componets/Greeting';
 import Welcome from './componets/Welcome';
+import MyComponets from './componets/MyComponets';
+//Nameed componets
+import { FirstComponets, SecondComponets } from './componets/MyComponets';
+import HelloWorld from './componets/HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <Welcome name = "chuman"/>
       <Greeting name ="suresh"/>
+      <FirstComponets />
+      <SecondComponets />
+      <MyComponets />
+      <HelloWorld />
     </div>
   );
 }
---
+++ src/componets/MyComponets.js
@@ -0,0 +1,13 @@
+
+export function FirstComponets(){
+    return <h1>first Components</h1>
+}
+
+
+export function SecondComponets(){
+    return <h1>second Components</h1>
+}
+
+export default function MyComponets(){
+    return <h1>My Components</h1>
+}
